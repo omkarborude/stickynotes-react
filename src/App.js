d93129a0bba16b8eb3b9a33fbc6f984bafe0e9ce
omkar <borudeomkar41@gmail.com>
2021-01-20 16:51:33 +0530

    Feat

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [getin , setgetin] = useState("");
+
+
+  // function Get Text in
+  function gettingIN(event) {
+    var textin = event.target.value;
+    setgetin(textin);
+  }
+
   return (
-    <div className="">
+    <div className="maindiv">
+
+      {/* create note button */}
+      <div className="cont1">
+      <button>
+        Create Note
+      </button>
+      </div>
+
+      {/* show written notes here  */}
+      <div className="cont2">
+
+      </div>
+
+      {/* show write note div */}
+      <div className="cont3">
+       <form>
+         <textarea id="getin" placeholder="Write Note here" onChange={gettingIN}>
+         </textarea>
+       </form>
+      </div>
+      
+
+
 
     </div>
   );
